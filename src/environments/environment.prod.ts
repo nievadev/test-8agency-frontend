b3f90440849460da4b47e85bf9c2dcdf967a5a15
Martín Nieva <martinjafactor@gmail.com>
2021-11-05 09:46:01 -0300

    Removed unnecessary files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  // Since this file is gonna be served by flask
-  // A better approach would be to serve the compiled frontend staticly by NGINX
-  // in something like a docker container, and have the API in another container with Gunicorn
-  apiUrl: window.location.origin
+  apiUrl: ''
 };

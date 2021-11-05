a92a15ec18407674afd264f0859f3f4b3cb8d737
Martín Nieva <martinjafactor@gmail.com>
2021-11-05 08:44:56 -0300

    Updated production environment to prepare for flask

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'domain.com/api/'
+  // Since this file is gonna be served by flask
+  // A better approach would be to serve the compiled frontend staticly by NGINX
+  // in something like a docker container, and have the API in another container with Gunicorn
+  apiUrl: window.location.origin
 };
